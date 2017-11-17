b02771bc38ad88fc9dc5936a150d87476e53ae9c
insideGen <joh13@laposte.net>
2017-11-17 17:56:09 +0100

    First commit

--- main.js
+++ main.js
@@ -1,60 +1,87 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const path = require("path");
+const {app, BrowserWindow, Menu, Tray} = require("electron");
+const log = require("electron-log");
+log.info("App starting...");
 
-const path = require('path')
-const url = require('url')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+// Global reference of the window object.
+let mainWindow = null;
+let contextMenu = null;
+let appTray = null;
 
-function createWindow () {
+let itemEnabled = true, itemChecked = false;
+
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  mainWindow = new BrowserWindow({
+    "width": 800,
+    "height": 600
+  });
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is going to be closed.
+  mainWindow.on("close", function (event) {
+    if (!app.isQuitting) {
+      //Calling event.preventDefault() will cancel the close.
+      event.preventDefault();
+      mainWindow.hide();
+    }
+  });
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+  mainWindow.on("closed", function () {
     mainWindow = null
-  })
+  });
+}
+
+function createTrayIcon() {
+  contextMenu = Menu.buildFromTemplate([
+    { id: "enabledTest", label: "Enabled test", type: "normal", enabled: itemEnabled, click: function (menuItem, browserWindow, event) {
+
+    } },
+    { id: "checkedTest", label: "Checked test", type: "checkbox", checked: itemChecked, click: function (menuItem, browserWindow, event) {
+      itemEnabled = !itemEnabled;
+      itemChecked = !itemChecked;
+    } },
+    { id: "exit", label: "Exit", type: "normal", enabled: true, click: function (menuItem, browserWindow, event) {
+      app.quit();
+    } }
+  ]);
+
+  appTray = new Tray(path.join(__dirname, "ressources/atom.ico"));
+  appTray.setToolTip(app.getName() + " v" + app.getVersion());
+  appTray.setContextMenu(contextMenu);
+  appTray.on("click", function (event, bounds) {
+    mainWindow.show();
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", function (launchInfo) {
+  createWindow();
+  createTrayIcon();
+});
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', function () {
+app.on('activate', function (event, hasVisibleWindows) {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on("browser-window-created", function (event, window) {
+  window.setMenu(null);
+});
+
+app.on("window-all-closed", function () {
+
+});
+
+app.on('before-quit', function (event) {
+  app.isQuitting = true;
+});
